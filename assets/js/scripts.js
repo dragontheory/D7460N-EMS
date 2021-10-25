ffe8bb34eab62e0154dbe6457826374e8e96f961
D7460N <80736+dragontheory@users.noreply.github.com>
2021-10-25 15:33:57 -0400

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    ActiveCSS.init({
+        configLocation: 'assets/activecss/config.txt, assets/activecss/conditionals.txt, assets/activecss/components.txt',
+    });
+});
