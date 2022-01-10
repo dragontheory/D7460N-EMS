fe32d1090495f9858af58ef2dd4d3719a38fafea
D7460N <80736+dragontheory@users.noreply.github.com>
2022-01-10 14:37:37 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', function(e) {
-    ActiveCSS.init({
-        configLocation: 'assets/activecss/config.acss, assets/activecss/conditionals.acss, assets/activecss/components.acss',
-    });
+  ActiveCSS.init({
+    configLocation: 'assets/activecss/config.acss, assets/activecss/conditionals.acss, assets/activecss/components.acss',
+  });
 });
 
 /* Output to console */
 function myExternalFunction(o) {
-    const li = document.querySelector(".active-li")
-	console.log(li.dataset)
+  const li = document.querySelector(".active-li")
+    console.log(li.dataset)
 }
