ed26a38cf39d8b02c27aaac943e7f6c0b25bb98d
D7460N <80736+dragontheory@users.noreply.github.com>
2022-01-10 17:05:21 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,13 @@ document.addEventListener('DOMContentLoaded', function(e) {
     configLocation: 'assets/activecss/config.acss, assets/activecss/conditionals.acss, assets/activecss/components.acss',
   });
 });
+
+//https://split.js.org/#/split-grid
+import Split from 'split-grid'
+
+Split({
+    columnGutters: [{
+        track: 1,
+        element: document.querySelector('.gutter-col-1'),
+    }],
+})
