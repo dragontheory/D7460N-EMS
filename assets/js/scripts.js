504d03363d29738278ea46e74981083046eef0db
D7460N <80736+dragontheory@users.noreply.github.com>
2022-01-10 16:05:19 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,9 +4,3 @@ document.addEventListener('DOMContentLoaded', function(e) {
     configLocation: 'assets/activecss/config.acss, assets/activecss/conditionals.acss, assets/activecss/components.acss',
   });
 });
-
-/* Output to console */
-/* function myExternalFunction(o) {
-  const li = document.querySelector(".active-li")
-    console.log(li.dataset)
-}*/
