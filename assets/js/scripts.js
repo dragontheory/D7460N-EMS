1141260aba806c0866d0b7c5b7935f2a1866b445
D7460N <80736+dragontheory@users.noreply.github.com>
2022-01-10 17:29:31 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,9 +7,30 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 // https://split.js.org/#/split-grid
 // https://github.com/nathancahill/split/tree/master/packages/splitjs
-window.Split({
+/*window.Split({
   columnGutters: [{
     track: 1,
     element: document.querySelector('.gutter-col-1'),
   }],
-})
+})*/
+
+Split(['.column03', '.column04'], {
+		snapOffset: 0, //turn off snap
+		sizes: [50, 50],
+		minSize: [300, 300],
+    expanedToMin: true,
+    elementStyle: function(dimension, size, gutterSize) {
+        return {
+            'flex-basis': 'calc(' + size + '% - ' + gutterSize + 'px)',
+					/*'width': 'calc(' + size + '% - ' + gutterSize + 'px)',
+					'min-width': 'calc(' + size + '% - ' + gutterSize + 'px)',
+					'max-width': 'calc(' + size + '% - ' + gutterSize + 'px)'*/
+        }
+    },
+    gutterStyle: function(dimension, gutterSize) {
+        return {
+            'flex-basis': gutterSize + 'px'
+        }
+    }
+});
+
