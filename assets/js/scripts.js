12884ff17121052c853f82b474bcaf7af057d6eb
D7460N <80736+dragontheory@users.noreply.github.com>
2022-01-11 14:52:36 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,9 +8,9 @@ document.addEventListener('DOMContentLoaded', function(e) {
 // https://split.js.org/#/split-grid
 // https://github.com/nathancahill/split/tree/master/packages/splitjs
 Split({
-	minSize: 100,
+  minSize: 100,
   columnGutters: [{
     track: 1,
-    element: document.querySelector('[data-gutter-col="1"]'),
+    element: document.querySelector('body input#layoutToggle02:checked ~ page-container > :nth-child(4)'),
   }],
 })
