7ca71d0038e5f1c318b91a44c93d9218af4279c4
D7460N <80736+dragontheory@users.noreply.github.com>
2022-02-22 14:40:45 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(e) {
   ActiveCSS.init({
-    configLocation: 'assets/activecss/config.acss, assets/activecss/conditionals.acss, assets/activecss/components.acss',
+    configLocation: 
+    'assets/activecss/components.acss,' + 
+    'assets/activecss/config.acss,' + 
+    'assets/activecss/conditionals.acss,' + 
+    'assets/activecss/closePanel.acss,' +
+    'assets/activecss/components/pageContainer.acss,' + 
+    'assets/activecss/components/asidePanel.acss,' + 
+    'assets/activecss/components/introPanel.acss,' + 
+    'assets/activecss/components/mainPanel.acss,' + 
+    'assets/activecss/components/navPanel.acss,' + 
+    'assets/activecss/components/pageFooter.acss,' + 
+    'assets/activecss/components/pageHeader.acss,' + 
+    'assets/activecss/components/pageLogo.acss,' + 
+    'assets/activecss/components/splitterPanel.acss',
   });
 });
