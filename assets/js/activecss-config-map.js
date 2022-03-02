780ec659ef0f5e0d6bb4deb8201613aa05b40e6b
D7460N <dragon@dragontheory.com>
2022-03-02 08:35:20 -0500

    Updating scaffolding

---
+++ assets/js/activecss-config-map.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+  ActiveCSS.init({
+    configLocation: 
+    'assets/activecss/components.acss,' + 
+    'assets/activecss/conditionals.acss,' + 
+    'assets/activecss/components/page-container/pageContainer.acss,' + 
+    'assets/activecss/components/page-header/pageHeader.acss,' +
+    'assets/activecss/components/page-logo/pageLogo.acss,' +    
+    'assets/activecss/components/intro-panel/introPanel.acss,' + 
+    'assets/activecss/components/nav-panel/navPanel.acss,' + 
+    'assets/activecss/components/main-panel/mainPanel.acss,' + 
+    'assets/activecss/components/splitter-panel/splitterPanel.acss,' +
+    'assets/activecss/components/aside-panel/asidePanel.acss,' + 
+    'assets/activecss/components/close-panel/closePanel.acss,' + 
+    'assets/activecss/components/page-footer/pageFooter.acss',
+  });
+});
