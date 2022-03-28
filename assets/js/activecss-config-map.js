34ff5a2bea2fd18a43113145cb1491bbd1369d72
D7460N <80736+dragontheory@users.noreply.github.com>
2022-03-28 15:35:37 -0400

    Update activecss-config-map.js

--- assets/js/activecss-config-map.js
+++ assets/js/activecss-config-map.js
@@ -2,23 +2,23 @@ document.addEventListener('DOMContentLoaded', function(e) {
   ActiveCSS.init({
     configLocation: 
     'assets/activecss/conditionals.acss,' + 
-    'assets/activecss/components/page-container/pageContainer.acss,' + 
-    'assets/activecss/components/page-header/pageHeader.acss,' +
-    'assets/activecss/components/page-search/pageSearch.acss,' +
-    'assets/activecss/components/list-dataset/listDataset.acss,' +
-    'assets/activecss/components/list-details/listDetails.acss,' +
-    'assets/activecss/components/list-graphics/listGraphics.acss,' +
-    'assets/activecss/components/list-layouts/listLayouts.acss,' +
-    'assets/activecss/components/list-pagination/listPagination.acss,' +
-    'assets/activecss/components/list-results/listResults.acss,' +
-    'assets/activecss/components/list-themes/listThemes.acss,' +
-    'assets/activecss/components/panel-intro/panelIntro.acss,' + 
-    'assets/activecss/components/panel-nav/panelNav.acss,' + 
-    'assets/activecss/components/panel-main/panelMain.acss,' + 
-    'assets/activecss/components/panel-main-count/panelMainCount.acss,' + 
-    'assets/activecss/components/panel-splitter/panelSplitter.acss,' +
-    'assets/activecss/components/panel-aside/panelAside.acss,' +
-    'assets/activecss/components/panel-aside2/panelAside2.acss,' +  
-    'assets/activecss/components/page-footer/pageFooter.acss',
+    'assets/activecss/components/page-container/page-container.acss,' + 
+    'assets/activecss/components/page-header/page-header.acss,' +
+    'assets/activecss/components/page-search/page-search.acss,' +
+    'assets/activecss/components/list-dataset/list-dataset.acss,' +
+    'assets/activecss/components/list-details/list-details.acss,' +
+    'assets/activecss/components/list-graphics/list-graphics.acss,' +
+    'assets/activecss/components/list-layouts/list-layouts.acss,' +
+    'assets/activecss/components/list-pagination/list-pagination.acss,' +
+    'assets/activecss/components/list-results/list-results.acss,' +
+    'assets/activecss/components/list-themes/list-themes.acss,' +
+    'assets/activecss/components/panel-intro/panel-intro.acss,' + 
+    'assets/activecss/components/panel-nav/panel-nav.acss,' + 
+    'assets/activecss/components/panel-main/panel-main.acss,' + 
+    'assets/activecss/components/panel-main-count/panel-main-count.acss,' + 
+    'assets/activecss/components/panel-splitter/panel-splitter.acss,' +
+    'assets/activecss/components/panel-aside/panel-aside.acss,' +
+    'assets/activecss/components/panel-aside2/panel-aside2.acss,' +  
+    'assets/activecss/components/page-footer/page-footer.acss',
   });
 });
