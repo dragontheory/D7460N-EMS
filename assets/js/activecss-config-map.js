f09f71c262527d5beb385202e40109702f479da5
D7460N <dragon@dragontheory.com>
2022-05-03 23:21:35 -0400

    migration

--- assets/js/activecss-config-map.js
+++ assets/js/activecss-config-map.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(e) {
   ActiveCSS.init({
     configLocation: 
-    'assets/activecss/conditionals.acss,' + 
-    'assets/activecss/components/page-container/page-container.acss,' + 
-    'assets/activecss/components/page-header/page-header.acss,' +
-    'assets/activecss/components/page-search/page-search.acss,' +
-    'assets/activecss/components/list-dataset/list-dataset.acss,' +
-    'assets/activecss/components/list-aside/list-aside.acss,' +
-    'assets/activecss/components/list-graphics/list-graphics.acss,' +
-    'assets/activecss/components/list-layouts/list-layouts.acss,' +
-    'assets/activecss/components/list-pagination/list-pagination.acss,' +
-    'assets/activecss/components/list-main/list-main.acss,' +
-    'assets/activecss/components/list-themes/list-themes.acss,' +
-    'assets/activecss/components/panel-intro/panel-intro.acss,' + 
-    'assets/activecss/components/panel-nav/panel-nav.acss,' + 
-    'assets/activecss/components/panel-main/panel-main.acss,' + 
-    'assets/activecss/components/panel-main-count/panel-main-count.acss,' + 
-    'assets/activecss/components/panel-splitter/panel-splitter.acss,' +
-    'assets/activecss/components/panel-aside/panel-aside.acss,' +
-    'assets/activecss/components/panel-aside2/panel-aside2.acss,' +  
-    'assets/activecss/components/page-footer/page-footer.acss',
+    'assets/conditionals.acss,' + 
+    'assets/components/page-container/page-container.acss,' + 
+    'assets/components/page-header/page-header.acss,' +
+    'assets/components/page-search/page-search.acss,' +
+    'assets/components/list-dataset/list-dataset.acss,' +
+    'assets/components/list-aside/list-aside.acss,' +
+    'assets/components/list-graphics/list-graphics.acss,' +
+    'assets/components/list-layouts/list-layouts.acss,' +
+    'assets/components/list-pagination/list-pagination.acss,' +
+    'assets/components/list-main/list-main.acss,' +
+    'assets/components/list-themes/list-themes.acss,' +
+    'assets/components/panel-intro/panel-intro.acss,' + 
+    'assets/components/panel-nav/panel-nav.acss,' + 
+    'assets/components/panel-main/panel-main.acss,' + 
+    'assets/components/panel-main-count/panel-main-count.acss,' + 
+    'assets/components/panel-splitter/panel-splitter.acss,' +
+    'assets/components/panel-aside/panel-aside.acss,' +
+    'assets/components/panel-aside2/panel-aside2.acss,' +  
+    'assets/components/page-footer/page-footer.acss',
   });
 });
